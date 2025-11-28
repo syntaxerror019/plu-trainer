2ec4731e06bc24e2c627204002e2187ffac26795
Miles Hilliard <skywiredvt@gmail.com>
2025-11-28 15:55:21 -0500

    initial commit

--- api/index.py
+++ api/index.py
@@ -1,11 +1,71 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+import csv
+import random    
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/practice')
+def practice():
+    return redirect('/')
+
+@app.route('/practice/plu/code', methods=['GET', 'POST'])
+def practice_plu_code():
+    iscorrect = None
+    previous_options = None
+    previous_correct_code = None
+    previous_name = None
+    
+    if request.method == 'POST':
+        correct_plu = request.form.get('correct_plu')
+        plu_code = request.form.get('plu_code')
+        iscorrect = True if plu_code == correct_plu else False
+        previous_options = request.form.get('options').split(',')
+        previous_correct_code = correct_plu
+        previous_name = request.form.get('name')
+        
+    name, correct_plu, options = generate_quiz_options()
+    
+    return render_template('quiz.html', iscorrect=iscorrect, correct=correct_plu, options=options, name=name, previous_options=previous_options, previous_correct_code=previous_correct_code, previous_name=previous_name)
+
+@app.route('/practice/plu/image')
+def practice_plu_image():
+    return render_template('quiz_image.html')
+
+@app.route('/practice/match')
+def practice_match():
+    return render_template('match.html')
+
+@app.route('/docs')
+def docs():
+    items = load_commodities()
+    return render_template('render.html', items=items)
+
+def load_commodities():
+    commodities = []
+    with open('commodities.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            commodities.append(row)
+    return commodities
+
+def generate_quiz_options():
+    items = load_commodities()
+    items = [i for i in items if "Retailer" not in i['Variety']]
+    
+    correct_item = random.choice(items)
+    name = f"{correct_item['Commodity'].title()} {correct_item['Variety'].title()}".strip()
+    correct_plu = correct_item['Plu']
+    wrong_items = random.sample([i for i in items if i['Plu'] != correct_plu], 3)
+
+    options = [correct_plu] + [i['Plu'] for i in wrong_items]
+    random.shuffle(options)
+
+    return name, correct_plu, options
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ api/static/search.js
@@ -0,0 +1,20 @@
+
+function searchTable() {
+    var input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementsByTagName("table")[0];
+    tr = table.getElementsByTagName("tr");    
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            txtValue = td[j].textContent || td[j].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                break;
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
